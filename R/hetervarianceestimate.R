e5e1e129ee1d8d0191e0b9f36bde6e60252f56c5
Zhang <andrew.haoyu@gmail.com>
2018-09-04 23:37:47 -0400

    hetervarianceestimate

--- R/hetervarianceestimate.R
+++ R/hetervarianceestimate.R
@@ -15,3 +15,24 @@ HeterVarianceEstimate <- function(log.odds,sigma){
   }
   return(result)
 }
+
+
+#' Title
+#'
+#' @param log.odds
+#' @param sigma
+#' @param R
+#'
+#' @return
+#' @export
+#'
+#' @examples
+HeterVarianceEstimateNew <- function(log.odds,sigma,R){
+  M <- length(log.odds)
+  result <- (sum((log.odds-mean(log.odds))^2)-sum(diag(sigma))+sum(sigma)/M)/(M-sum(R)/M)
+  #result <- (sum((log.odds-mean(log.odds))^2)-(M+1)*sum(diag(sigma))/M)/(M-1)
+  #if(result <= 0){
+  #  result <- 0
+  # }
+  return(result)
+}
